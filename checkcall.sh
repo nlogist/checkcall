a268c425d9491377cde7e689501b6df5c689ddcf
nlogist <root@diskstation>
2019-05-15 13:28:31 +0900

    Added: VALUE2 and VALUE3

--- checkcall.sh
+++ checkcall.sh
@@ -13,6 +13,22 @@ if [ "$CURRENTSTAT" = "" ]; then
 fi
 
 if [ "$PREVIOUSSTAT" != "$CURRENTSTAT" ]; then
-  wget -q -O - "http://maker.ifttt.com/trigger/$EVENT/with/key/$SECRET_KEY?value1=$CURRENTSTAT" > /dev/null 2>&1
+  GLOBIGNORE=*
+  TIME_CUR=`echo $CURRENTSTAT | cut -d ' ' -f 2-6`
+  TIME_BEGIN=`echo $CURRENTSTAT | cut -d ' ' -f 7-11`
+  TIME_END=`echo $CURRENTSTAT | cut -d ' ' -f 12-16 | cut -d '<' -f 1`
+  INFO=`echo ${CURRENTSTAT:78} | perl -pe 's/<br>//g; s/\s+/ /g; s/^\s+//'`
+  unset GLOBIGNORE
+
+  TIME_BEGIN_SEC=`date -d "$TIME_BEGIN" +"%s"`
+  TIME_END_SEC=`date -d "$TIME_END" +"%s"`
+
+  DURATION_SEC=`expr $TIME_END_SEC - $TIME_BEGIN_SEC`
+  DURATION_HMS=`date -u -d @$DURATION_SEC +"%T"`
+  VALUE1="日時: $TIME_CUR"
+  VALUE2="通話時間: $DURATION_HMS"
+  VALUE3="備考: $INFO"
+
+  wget -q -O - "http://maker.ifttt.com/trigger/$EVENT/with/key/$SECRET_KEY?value1=$VALUE1&value2=$VALUE2&value3=$VALUE3" > /dev/null 2>&1
   echo "$CURRENTSTAT" >> $LOGFILE
 fi
